1c885b50bde53738dbe44311ea377f3aa838bd9c
Crayon2000 <crayon1@rocketmail.com>
2009-03-09 02:43:04 +0000

    [CHG] Using GRRLIB 4.0.0 revision 76 [NEW] Button class as been improved (Home button and big button)
    git-svn-id: https://wii-tac-toe.googlecode.com/svn@25 bba1cac1-252d-79fd-420d-80adb1f9fa09

--- source/button.cpp
+++ source/button.cpp
@@ -2,6 +2,8 @@
 #include <gccore.h>
 #include "../gfx/button_on.h"
 #include "../gfx/button_off.h"
+#include "../gfx/home_button.h"
+#include "../gfx/button_home.h"
 #include "button.h"
 
 /**
@@ -9,18 +11,18 @@
  */
 Button::Button() : Object()
 {
-	Selected = false;
-	Left = 0;
-	Top = 0;
-	Width = 400;
-	Height = 64;
+    this->Type = btnStdMenu;
+    Init();
+}
 
-	TextColor = 0x000000;
-	TextHeight = 12;
-	TextWidth = 100; // random value
-
-	ButtonImgOn = GRRLIB_LoadTexture(button_on);
-	ButtonImgOff = GRRLIB_LoadTexture(button_off);
+/**
+ * Constructor for the Button class.
+  * @param[in] Type Button type.
+ */
+Button::Button(buttonType Type) : Object()
+{
+    this->Type = Type;
+    Init();
 }
 
 /**
@@ -28,8 +30,42 @@ Button::Button() : Object()
  */
 Button::~Button()
 {
-	free(ButtonImgOn.data);
-	free(ButtonImgOff.data);
+    if(ButtonImgOn.data)
+        free(ButtonImgOn.data);
+    if(ButtonImgOff.data)
+        free(ButtonImgOff.data);
+}
+
+/**
+ * Initialize the Button class.
+ */
+void Button::Init()
+{
+    switch(Type)
+    {
+        case btnHomeMenu:
+            ButtonImgOn.data = NULL;
+            ButtonImgOff = GRRLIB_LoadTexture(button_home);
+            break;
+        case btnHome:
+            ButtonImgOn.data = NULL;
+            ButtonImgOff = GRRLIB_LoadTexture(home_button);
+            break;
+        default:
+            ButtonImgOn = GRRLIB_LoadTexture(button_on);
+            ButtonImgOff = GRRLIB_LoadTexture(button_off);
+    }
+
+	Selected = false;
+
+	Left = 0;
+	Top = 0;
+	Width = ButtonImgOff.w;
+	Height = ButtonImgOff.h;
+
+	TextColor = 0x000000;
+	TextHeight = 14;
+	TextWidth = 100; // random value
 }
 
 /**
@@ -37,12 +73,29 @@ Button::~Button()
  */
 void Button::Paint()
 {
-	GRRLIB_DrawImg(Left, Top, ButtonImgOff, 0, 1.0, 1.0, 0xFFFFFFFF);
+    if(Type == btnHomeMenu)
+    {   // Draw shadow
+        GRRLIB_DrawImg(Left + 4, Top + 5, ButtonImgOff, 0, 1.0, 1.0, 0x00000055);
+    }
+    GRRLIB_DrawImg(Left, Top, ButtonImgOff, 0, 1.0, 1.0, 0xFFFFFFFF);
 	GRRLIB_Printf2(TextLeft, TextTop, Caption, TextHeight, TextColor);
-	if(Selected)
-	{
+	if(Type == btnStdMenu && Selected)
+	{   // Hover color
 		GRRLIB_DrawImg(Left, Top, ButtonImgOn, 0, 1.0, 1.0, 0xFFFFFFFF);
 	}
+    else if(Type == btnHomeMenu && Selected)
+    {   // Hover color
+        GRRLIB_DrawImg(Left, Top, ButtonImgOff, 0, 1.0, 1.0, 0x0000FF33);
+    }
+}
+
+/**
+ * Set text height of the button.
+ * @param[in] Text height of the button.
+ */
+void Button::SetTextHeight(unsigned int TextHeight)
+{
+    this->TextHeight = TextHeight;
 }
 
 /**
@@ -55,6 +108,10 @@ void Button::SetCaption(const char *Caption)
 	TextWidth = GRRLIB_TextWidth(this->Caption, TextHeight);
 	TextTop = Top + (Height / 2) - (TextHeight / 2);
 	TextLeft = Left + (Width / 2) - (TextWidth / 2);
+    if(Type == btnHome)
+    {
+        TextLeft += 20;
+    }
 }
 
 /**
--- source/button.h
+++ source/button.h
@@ -6,6 +6,15 @@
 #include "object.h"
 
 /**
+ * Types of button that could be used.
+ */
+enum buttonType {
+    btnHome,     /**< Home button with a small house. */
+    btnHomeMenu, /**< Button to go in the HOME menu. A shadow is always present under the button. */
+    btnStdMenu   /**< Button to go in a menu. Change color when the cursor over the button. */
+};
+
+/**
  * This a class to make a button on the screen with two states (selected or not).
  * @author Crayon
  */
@@ -13,12 +22,15 @@ class Button : public Object
 {
 public:
 	Button();
+	Button(buttonType);
 	~Button();
 	void Paint();
 	void SetCaption(const char *);
 	void SetSelected(bool);
 	void SetTextColor(u32);
+    void SetTextHeight(unsigned int);
 private:
+    void Init();
 	bool Selected;
 	char Caption[50 + 1];
 	unsigned int TextWidth;
@@ -26,6 +38,7 @@ private:
 	unsigned int TextTop;
 	unsigned int TextLeft;
 	u32 TextColor;
+    buttonType Type;
 	GRRLIB_texImg ButtonImgOn;
 	GRRLIB_texImg ButtonImgOff;
 };
