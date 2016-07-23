1a745108aa56ec5e69b2fedce384f75cb1abd16d
Crayon2000 <crayon1@rocketmail.com>
2016-07-22 21:51:33 -0400

    Updated libjpeg to version 9b of 17-Jan-2016.

--- source/libjpeg/jconfig.h
+++ source/libjpeg/jconfig.h
@@ -5,42 +5,42 @@
 #define HAVE_PROTOTYPES 1
 #define HAVE_UNSIGNED_CHAR 1
 #define HAVE_UNSIGNED_SHORT 1
-#undef void
-#undef const
-#undef CHAR_IS_UNSIGNED
+/* #undef void */
+/* #undef const */
+/* #undef CHAR_IS_UNSIGNED */
 #define HAVE_STDDEF_H 1
 #define HAVE_STDLIB_H 1
 #define HAVE_LOCALE_H 1
-#undef NEED_BSD_STRINGS
-#undef NEED_SYS_TYPES_H
-#undef NEED_FAR_POINTERS
-#undef NEED_SHORT_EXTERNAL_NAMES
+/* #undef NEED_BSD_STRINGS */
+/* #undef NEED_SYS_TYPES_H */
+/* #undef NEED_FAR_POINTERS */
+/* #undef NEED_SHORT_EXTERNAL_NAMES */
 /* Define this if you get warnings about undefined structures. */
-#undef INCOMPLETE_TYPES_BROKEN
+/* #undef INCOMPLETE_TYPES_BROKEN */
 
 #ifdef JPEG_INTERNALS
 
-#undef RIGHT_SHIFT_IS_UNSIGNED
+/* #undef RIGHT_SHIFT_IS_UNSIGNED */
 #define INLINE __inline__
 /* These are for configuring the JPEG memory manager. */
-#undef DEFAULT_MAX_MEM
-#undef NO_MKTEMP
+/* #undef DEFAULT_MAX_MEM */
+/* #undef NO_MKTEMP */
 
 #endif /* JPEG_INTERNALS */
 
 #ifdef JPEG_CJPEG_DJPEG
 
-#undef BMP_SUPPORTED		/* BMP image file format */
-#undef GIF_SUPPORTED		/* GIF image file format */
-#undef PPM_SUPPORTED		/* PBMPLUS PPM/PGM image file format */
-#undef RLE_SUPPORTED
-#undef TARGA_SUPPORTED		/* Targa image file format */
+/* #undef BMP_SUPPORTED */		/* BMP image file format */
+/* #undef GIF_SUPPORTED */		/* GIF image file format */
+/* #undef PPM_SUPPORTED */		/* PBMPLUS PPM/PGM image file format */
+/* #undef RLE_SUPPORTED */
+/* #undef TARGA_SUPPORTED */		/* Targa image file format */
 
-#undef TWO_FILE_COMMANDLINE
-#undef NEED_SIGNAL_CATCHER
-#undef DONT_USE_B_MODE
+/* #undef TWO_FILE_COMMANDLINE */
+/* #undef NEED_SIGNAL_CATCHER */
+/* #undef DONT_USE_B_MODE */
 
 /* Define this if you want percent-done progress reports from cjpeg/djpeg. */
-#undef PROGRESS_REPORT
+/* #undef PROGRESS_REPORT */
 
 #endif /* JPEG_CJPEG_DJPEG */
