6ec99c5159da96d97407384976d3cebcf7578aaf
Crayon <crayon1@rocketmail.com>
2024-07-11 00:53:23 -0400

    Use ninja 1.12.1

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,7 +4,12 @@ set -ex
 # Remove installed version of cmake
 apt-get purge cmake -y
 
-# Install required version
+# Install required cmake version
 curl --location https://github.com/Kitware/CMake/releases/download/v3.30.0/cmake-3.30.0-linux-x86_64.sh --output /tmp/cmake.sh
 sh /tmp/cmake.sh --skip-license --prefix=/usr
 rm -rf /tmp/cmake.sh
+
+# Install required ninja version
+curl --location https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip --output /usr/bin/ninja.gz
+gunzip --verbose --force /usr/bin/ninja.gz
+chmod a+x /usr/bin/ninja
